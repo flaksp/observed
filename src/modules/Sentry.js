8ecf334be7b62f844a3d4ea28abfc43a78364f6c
Petr Luzhin <lithx@yandex.com>
2018-03-04 14:39:47 +0300

    Remove old files

--- src/modules/Sentry.js
+++
@@ -1,3 +0,0 @@
-export default function () {
-  window.Raven.config(process.env.SENTRY_DSN).install();
-}
--- src/modules/YandexMetrika.js
+++
@@ -1,9 +0,0 @@
-export default function () {
-  window.yaCounter = new window.Ya.Metrika({
-    id: process.env.YANDEX_METRIKA_ID,
-    clickmap: true,
-    trackLinks: true,
-    accurateTrackBounce: true,
-    trackHash: true,
-  });
-}
